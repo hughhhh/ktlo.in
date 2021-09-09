28b857fd026292be521a50f294e6fd86e76a7a7a
Hugh A. Miles II <hughmil3s@gmail.com>
2021-09-09 22:05:00 +0300

    Fix layout

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { format } from "d3-format";
 import "./styles.css";
 
 const SECONDS_IN_YEAR = 1 / 31536000;
+const SECONDS_IN_DAY = 1 / 86400;
+
 const MILLISECONDS_IN_YEAR = SECONDS_IN_YEAR / 1000;
+const MILLISECONDS_IN_DAY = SECONDS_IN_DAY / 1000;
 const AGE_FORMAT = ".20s";
 
 const formatTime = (temps) => format(AGE_FORMAT)(temps);
@@ -13,17 +16,16 @@ const formatTime = (temps) => format(AGE_FORMAT)(temps);
 class App extends React.Component {
   constructor(props) {
     super(props);
-    const launchDate =
-      -1 * (moment().unix() - moment("20210929", "YYYYMMDD").unix());
+    const launchDate = moment().unix() - moment("20210929", "YYYYMMDD").unix();
 
     this.state = {
-      launchDate: launchDate * SECONDS_IN_YEAR
+      launchDate: launchDate * SECONDS_IN_DAY
     };
 
     this.timer = setInterval(
       () =>
         this.setState({
-          launchDate: this.state.launchDate + MILLISECONDS_IN_YEAR
+          launchDate: Math.abs(this.state.launchDate - MILLISECONDS_IN_DAY)
         }),
       1
     );
@@ -37,9 +39,15 @@ class App extends React.Component {
         <p>
           Keeping the lights on presents a 5 day experiment with the best
           upcoming artist. Creating an environment from them to collaborate and
-          produce the best music. Join us for a spectacle in time.
+          produce the best music.
         </p>
-        {formatTime(launchDate)}
+
+        <p>
+          <b>Featuring x</b> @shilla @vern @swellshark @georgenaux
+        </p>
+        <p>Join us for a spectacle in time ✨</p>
+        <h3>Launch</h3>
+        <span>{formatTime(launchDate)} days</span>
       </div>
     );
   }
