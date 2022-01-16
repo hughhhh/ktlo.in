639890300e8f47a45516044d92b6914aa7fefcf2
Hugh A. Miles <hugh@Hughs-MacBook-Pro.local>
2022-01-15 20:27:05 -0500

    add link for mirror post

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: '/',
+        destination: 'https://mirror.xyz/hughmiles.eth/PCTWxkJ5goOS0bRVG-UqDTglPxyhBKGDoPKiKCL6JN4',
+        permanent: false,
+        basePath: false
+      },
+      {
         source: '/one',
         destination: 'https://0xhugh.mintgate.io/go/3SwzVDqtVyGI/',
         permanent: false,
