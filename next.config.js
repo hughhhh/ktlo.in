2618605263151f6df3971822fea3f8aa893344e8
Hugh A. Miles <hugh@Hughs-MacBook-Pro.local>
2022-01-15 20:16:55 -0500

    add path to mirror

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/one',
+        destination: 'https://0xhugh.mintgate.io/go/3SwzVDqtVyGI/',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
   reactStrictMode: true,
-}
+};
